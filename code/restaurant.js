97eaf4c724562d2a2cc0283abb22420b26fa6ea8
Ragna <ragnaulfars@gmail.com>
2020-02-24 16:09:36 +0100

    Working on sorting feature of ratings

--- code/restaurant.js
+++ code/restaurant.js
@@ -3,6 +3,8 @@ const apiKey = "4fd2dd29c14db73b1a3c8639ecc045bb"
 const cityName = 61
 const cuisineType = 60
 const apiData = `https://developers.zomato.com/api/v2.1/search?entity_id=${cityName}&entity_type=city&cuisines=${cuisineType}`
+let orderRatings = [] 
+
 
 // DOM
 const foodReview = document.getElementById("foodReview") 
@@ -15,6 +17,16 @@ fetch(apiData, {headers:{ "user-key": apiKey}})
     console.log(json)
 
     // restaurantName.innerHTML = `<p>The Restaurant: ${json.restaurants[6].restaurant.name}</p>`
+    
+    orderRatings = json.restaurants[0].restaurant.user_rating.aggregate_rating
+      console.log(orderRatings)
+
+    const sortRatings = () => {
+      orderRatings.sort((a,b) =>{
+        return a - b 
+      })
+    }
+
 
     json.restaurants.forEach(item => {
       let restaurantName = item.restaurant.name
@@ -39,5 +51,16 @@ fetch(apiData, {headers:{ "user-key": apiKey}})
       <p>${averageCost}</p>
       <p>${ratings}/5.0</p>
       </div>`
+
+      
+     
     })
+
+    
+    
+
+    
+  
   })
+
+  

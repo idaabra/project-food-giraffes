62b929fd6181044149512256e2ed3412c2df432e
Ragna <ragnaulfars@gmail.com>
2020-02-25 13:03:22 +0100

    Sorting completed

--- code/restaurant.js
+++ code/restaurant.js
@@ -4,6 +4,19 @@ const cityName = 61
 const cuisineType = 60
 const apiData = `https://developers.zomato.com/api/v2.1/search?entity_id=${cityName}&entity_type=city&cuisines=${cuisineType}`
 
+let restaurantsArrays
+
+// FUNCTIONS
+
+
+
+const sortRatings = () => {
+  restaurantsArrays.sort((a,b) => {
+    return b.restaurant.user_rating.aggregate_rating - a.restaurant.user_rating.aggregate_rating
+  })
+}
+
+
 
 // DOM
 const foodReview = document.getElementById("foodReview") 
@@ -15,50 +28,45 @@ fetch(apiData, {headers:{ "user-key": apiKey}})
   .then((json) => {
     console.log(json)
 
-     
+    restaurantsArrays = json.restaurants
 
-    const sortRatings = () => {
-      json.restaurants.sort((a,b) =>{
-        return b.restaurant.user_rating.aggregate_rating - a.restaurant.user_rating.aggregate_rating 
-      })
-    }
-
-    sortRatings()
-
-    json.restaurants.forEach(item => {
-
-      let restaurantName = item.restaurant.name
-      console.log(restaurantName)
-
-      let averageCost = item.restaurant.average_cost_for_two
-      console.log(averageCost)
-
-      let address = item.restaurant.location.address
-      console.log(address)
-
-      let restImage = item.restaurant.photos[0].photo.url
-      console.log(restImage)
-
-      let ratings = item.restaurant.user_rating.aggregate_rating
-      console.log(ratings)
-
-      foodReview.innerHTML += `<div id="children">
-      <img src="${restImage}"></img>
-      <p>${restaurantName}</p>
-      <p>${address}</p>
-      <p>${averageCost}</p>
-      <p>${ratings}/5.0</p>
-      </div>`
-
-      
-     
-    })
-
-    
-    
-
+      printArray(json.restaurants) 
     
   
   })
 
-  
+  const printArray = (array) => {
+   foodReview.innerHTML = ""
+    array.forEach(item => {
+    let restaurantName = item.restaurant.name
+    // console.log(restaurantName)
+
+    let averageCost = item.restaurant.average_cost_for_two
+    // console.log(averageCost)
+
+    let address = item.restaurant.location.address
+    // console.log(address)
+
+    let restImage = item.restaurant.photos[0].photo.url
+    // console.log(restImage)
+
+    let ratings = item.restaurant.user_rating.aggregate_rating
+    // console.log(ratings)
+
+  foodReview.innerHTML += `<div id="children">
+  <img src="${restImage}" height="100"></img>
+  <p>${restaurantName}</p>
+  <p>${address}</p>
+  <p>${averageCost}</p>
+  <p>${ratings}/5.0</p>
+  </div>`
+   })
+}
+
+const skrivut = () => {
+  console.log("skriv ut")
+  sortRatings()
+  printArray(restaurantsArrays)
+} 
+
+document.getElementById('sortRating').onclick = skrivut

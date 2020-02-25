9e3430685411429b8283f4ce3b8e12e7078b076a
Ragna <ragnaulfars@gmail.com>
2020-02-25 10:01:29 +0100

    The sort-rating works.

--- code/restaurant.js
+++ code/restaurant.js
@@ -3,7 +3,6 @@ const apiKey = "4fd2dd29c14db73b1a3c8639ecc045bb"
 const cityName = 61
 const cuisineType = 60
 const apiData = `https://developers.zomato.com/api/v2.1/search?entity_id=${cityName}&entity_type=city&cuisines=${cuisineType}`
-let orderRatings = [] 
 
 
 // DOM
@@ -16,19 +15,18 @@ fetch(apiData, {headers:{ "user-key": apiKey}})
   .then((json) => {
     console.log(json)
 
-    // restaurantName.innerHTML = `<p>The Restaurant: ${json.restaurants[6].restaurant.name}</p>`
-    
-    orderRatings = json.restaurants[0].restaurant.user_rating.aggregate_rating
-      console.log(orderRatings)
+     
 
     const sortRatings = () => {
-      orderRatings.sort((a,b) =>{
-        return a - b 
+      json.restaurants.sort((a,b) =>{
+        return b.restaurant.user_rating.aggregate_rating - a.restaurant.user_rating.aggregate_rating 
       })
     }
 
+    sortRatings()
 
     json.restaurants.forEach(item => {
+
       let restaurantName = item.restaurant.name
       console.log(restaurantName)
 
